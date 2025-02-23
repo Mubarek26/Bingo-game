9cd07f7112a43da3f0ef168f734a74fcd735a81a
Nife Esleman <nsr-1868-14@amu.edu.et>
2025-02-23 14:04:09 +0300

    Rout for Bingopopup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,21 @@
-import { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './App.css';
-import Bet from './pages/Bet';
-import SelectCards from './pages/SelectCards';
-import Bingoboard from './pages/Bingoboard'
-import BingoCaller from './pages/BingoCaller';
+import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Bet from "./pages/Bet";
+import SelectCards from "./pages/SelectCards";
+import Bingoboard from "./pages/Bingoboard";
+import BingoCaller from "./pages/BingoCaller";
+import Bingopopup from "./pages/Bingopopup";
 function App() {
   return (
     <>
-      
       <Routes>
         <Route path="/BetAmountPage" element={<Bet />} />
         <Route path="/SelectCards" element={<SelectCards />} />
-        <Route path='/Bingoboard' element={<Bingoboard/> } />
-        <Route path='/Bingocaller' element={<BingoCaller/> } />
+        <Route path="/Bingoboard" element={<Bingoboard />} />
+        <Route path="/Bingopopup" element={<Bingopopup />} />
       </Routes>
-   </>
-   
+    </>
   );
 }
 
