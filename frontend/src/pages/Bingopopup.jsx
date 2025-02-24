3fca9e932d7272938747f280cf79a6500b9a9935
Nife Esleman <nsr-1868-14@amu.edu.et>
2025-02-24 21:01:48 +0300

    bingopopup latest version

--- frontend/src/pages/Bingopopup.jsx
+++ frontend/src/pages/Bingopopup.jsx
@@ -1,79 +1,18 @@
 import React from "react";
+import "./BingoPopup.css";
 
 const BingoPopup = () => {
-  const styles = {
-    popupContainer: {
-      position: "fixed",
-      top: 0,
-      left: 0,
-      width: "100%",
-      height: "100%",
-      backgroundColor: "rgba(0, 0, 0, 0.5)",
-      display: "flex",
-      justifyContent: "center",
-      alignItems: "center",
-    },
-    popupContent: {
-      backgroundColor: "#FFD700",
-      padding: "20px",
-      borderRadius: "10px",
-      textAlign: "center",
-      position: "relative",
-      width: "400px",
-    },
-    popupHeader: {
-      fontSize: "20px",
-      fontWeight: "bold",
-      color: "purple",
-    },
-    promoImage: {
-      width: "100%",
-      borderRadius: "5px",
-    },
-    closeButton: {
-      position: "absolute",
-      top: "10px",
-      right: "10px",
-      background: "red",
-      color: "purple",
-      border: "none",
-      padding: "5px 10px",
-      cursor: "pointer",
-    },
-    prizeTitle: {
-      fontSize: "18px",
-      fontWeight: "bold",
-      marginTop: "10px",
-      color: "purple",
-    },
-    prizes: {
-      display: "flex",
-      justifyContent: "space-around",
-      marginTop: "10px",
-    },
-    prizeColumn: {
-      textAlign: "left",
-      color: "purple",
-      fontSize: "16px ",
-      fontWeight: "bold",
-    },
-    socialLinks: {
-      marginTop: "10px",
-      fontSize: "14px",
-    },
-  };
-
   return (
-    <div style={styles.popupContainer}>
-      <div style={styles.popupContent}>
-        <h2 style={styles.popupHeader}>እቃዎች ቀላልነት ያላቸው ዝግጅት</h2>
+    <div className="popup-container">
+      <div className="popup-content">
+        <h2 className="popup-header">እቃዎች ቀላልነት ያላቸው ዝግጅት</h2>
         <img
           src="/frontend/public/images/ahun.jpg"
           alt="Bingo Promotion"
-          style={styles.promoImage}
+          className="promo-image"
         />
-        <div style={styles.prizes}>
-          <div style={styles.prizeColumn}>
+        <div className="prizes">
+          <div className="prize-column">
             <ul className="prize-list">
               <li>1 ጨዋታ = 1 ነጥብ ይቀላቀሉ </li>
               <li>ተጫውተው ብቻ ይሸለሙ</li>
@@ -87,13 +26,7 @@ const BingoPopup = () => {
             </ul>
           </div>
         </div>
-        <div style={styles.socialLinks}>
-          <p>@AHUNBINGO_BOT | @AHUNBINGO_CHAT</p>
-          <p>
-            <a href="https://www.ahungames.com">www.ahungames.com</a>
-          </p>
-        </div>
-        <button style={styles.closeButton} onClick={() => alert("Close Popup")}>
+        <button className="close-button" onClick={() => alert("Close Popup")}>
           X
         </button>
       </div>
